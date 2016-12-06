9384f5835bd4e551d57eea9be5c003237cccc0b3
Constantine Lebedev <koss.lebedev@gmail.com>
2016-12-06 13:37:01 +0500

    2016 Day 06

---
+++ 2016/06/part2.rb
@@ -0,0 +1,10 @@
+data = []
+8.times { data << Hash.new(0) }
+
+File.readlines('./input.txt').each do |line|
+  line.strip.each_char.with_index do |char, index|
+    data[index][char] += 1
+  end
+end
+
+puts data.map { |hash| hash.sort_by { |k, v| v }[0][0] }.join
