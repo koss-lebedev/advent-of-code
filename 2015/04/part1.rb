8728f190d22ee52b021d5980766db3b29edee8da
Constantine Lebedev <koss.lebedev@gmail.com>
2016-12-16 20:03:55 +0500

    2015 Day 04 Full

---
+++ 2015/04/part1.rb
@@ -0,0 +1,16 @@
+require 'digest'
+
+key = 'bgvyzdsv'
+
+index = 0
+
+8.times do
+  loop do
+    hash = Digest::MD5.hexdigest "#{key}#{index}"
+    break if hash.start_with?('00000')
+    index += 1
+  end
+end
+
+
+puts index
