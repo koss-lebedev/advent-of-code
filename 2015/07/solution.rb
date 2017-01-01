a591e1f99425cef9589932e54cf8d7523361b60a
Constantine Lebedev <koss.lebedev@gmail.com>
2017-01-01 19:51:49 +0500

    2015 Day 07 Full

--- 2015/07/solution.rb
+++ 2015/07/solution.rb
@@ -1,67 +1,54 @@
-def execute(commands, initial_state = {})
-  wires = Hash.new.merge(initial_state)
+class Puzzle
 
-  until commands.empty?
-    command = commands.shift
-    case command
-      when /LSHIFT/
-        matches = command.match(/(?<a>\w+) LSHIFT (?<b>\w+) -> (?<reg>\w+)/)
-        a = (Integer(matches[:a]) rescue wires[matches[:a]])
-        if a
-          wires[matches[:reg]] = a << Integer(matches[:b])
-        else
-          commands.push command
-        end
-      when /RSHIFT/
-        matches = command.match(/(?<a>\w+) RSHIFT (?<b>\w+) -> (?<reg>\w+)/)
-        a = (Integer(matches[:a]) rescue wires[matches[:a]])
-        if a
-          wires[matches[:reg]] = a >> Integer(matches[:b])
-        else
-          commands.push command
-        end
-      when /AND/
-        matches = command.match(/(?<a>\w+) AND (?<b>\w+) -> (?<reg>\w+)/)
-        a = (Integer(matches[:a]) rescue wires[matches[:a]])
-        b = (Integer(matches[:b]) rescue wires[matches[:b]])
-        if a && b
-          wires[matches[:reg]] = a & b
-        else
-          commands.push command
-        end
-      when /OR/
-        matches = command.match(/(?<a>\w+) OR (?<b>\w+) -> (?<reg>\w+)/)
-        a, b = (Integer(matches[:a]) rescue wires[matches[:a]]), (Integer(matches[:b]) rescue wires[matches[:b]])
-        if a && b
-          wires[matches[:reg]] = a | b
-        else
-          commands.push command
-        end
-      when /NOT/
-        matches = command.match(/NOT (?<a>\w+) -> (?<reg>\w+)/)
-        a = (Integer(matches[:a]) rescue wires[matches[:a]])
-        if a
-          wires[matches[:reg]] = a ^ 0xffff
-        else
-          commands.push command
-        end
-      else
-        matches = command.match(/(?<arg>\w+) -> (?<reg>\w+)/)
-        a = Integer(matches[:arg]) rescue wires[matches[:arg]]
-        if a
-          wires[matches[:reg]] = a
-        else
-          commands.push command
-        end
+  def initialize
+    @wires = Hash.new
+  end
+
+  def solve(commands, initial_state = {})
+    prepare commands
+    wires.merge! initial_state
+    resolve 'a'
+  end
+
+private
+
+  attr_reader :wires
+
+  def prepare(commands)
+    commands.each do |command|
+      op, wire = command.split(' -> ').map(&:strip)
+      wires[wire] = op
     end
   end
 
+  def resolve(wire)
+    Integer(wire)
+  rescue
+    case wires[wire]
+      when /LSHIFT/
+        a, b = wires[wire].split(' LSHIFT ')
+        wires[wire] = resolve(a) << resolve(b)
+      when /RSHIFT/
+        a, b = wires[wire].split(' RSHIFT ')
+        wires[wire] = resolve(a) >> resolve(b)
+      when /AND/
+        a, b = wires[wire].split(' AND ')
+        wires[wire] = resolve(a) & resolve(b)
+      when /OR/
+        a, b = wires[wire].split(' OR ')
+        wires[wire] = resolve(a) | resolve(b)
+      when /NOT/
+        a = wires[wire][4..-1]
+        wires[wire] = resolve(a) ^ 0xFFFF
+      else
+        resolve(wires[wire])
+    end
+  end
 
-  wires['a']
 end
 
-part1 = execute File.readlines('./input.txt')
+part1 = Puzzle.new.solve File.readlines('./input.txt')
 puts part1
 
-part2 = execute File.readlines('./input.txt'), { 'b' => part1 }
+part2 = Puzzle.new.solve File.readlines('./input.txt'), { 'b' => part1 }
 puts part2
