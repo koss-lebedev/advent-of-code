58171b165c8d4440c15235df6ceaad665c46fb87
Constantine Lebedev <koss.lebedev@gmail.com>
2017-01-11 21:57:24 +0500

    2016 Day 17 Part 1

---
+++ 2016/17/part1.rb
@@ -0,0 +1,44 @@
+require 'digest'
+
+class Point < Struct.new(:x, :y, :path);
+  def ==(other)
+    x == other.x && y == other.y
+  end
+end
+
+def possible_moves(point, code)
+  moves = []
+
+  dirs = Digest::MD5.hexdigest(code + point.path)[0...4].chars.map { |char| char.ord > 97 ? true : false }
+
+  if dirs[1] && point.y < 3
+    moves << Point.new(point.x, point.y + 1, point.path + ?D)
+  end
+  if dirs[3] && point.x < 3
+    moves << Point.new(point.x + 1, point.y, point.path + ?R)
+  end
+  if dirs[0] && point.y > 0
+    moves << Point.new(point.x, point.y - 1, point.path + ?U)
+  end
+  if dirs[2] && point.x > 0
+    moves << Point.new(point.x - 1, point.y, point.path + ?L)
+  end
+
+  moves
+end
+
+code = 'awrkjxxr'.freeze
+queue = [Point.new(0, 0, '')]
+dest = Point.new(3,3, nil)
+
+until queue.empty?
+  current = queue.shift
+
+  if current == dest
+    puts current.path
+    break
+  end
+
+  possible_moves(current, code).each { |move| queue << move }
+
+end
