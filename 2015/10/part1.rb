9180af7eeb52dac0b40406002f087d7c9892422f
Constantine Lebedev <koss.lebedev@gmail.com>
2017-01-22 19:41:04 +0500

    2015 Day 10 Part 1

---
+++ 2015/10/part1.rb
@@ -0,0 +1,20 @@
+def look_and_say_seq(input, times)
+  if times.zero?
+    input
+  else
+    new_seq = ''
+    pointer = 0
+    while pointer < input.size
+      counter = 1
+      while input[pointer + counter - 1] == input[pointer + counter]
+        counter += 1
+      end
+      new_seq += "#{counter}#{input[pointer]}"
+      pointer += counter
+    end
+    puts times
+    look_and_say_seq(new_seq, times - 1)
+  end
+end
+
+puts look_and_say_seq('1113122113', 40).size
