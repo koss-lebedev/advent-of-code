4f4578bddcb88a0280d5925a97dc28e6c7f4222b
Constantine Lebedev <koss.lebedev@gmail.com>
2017-02-17 01:25:26 +0500

    2015 Day 01 Part 2 Elixir

---
+++ 2015/01/part2.ex
@@ -0,0 +1,22 @@
+defmodule AdventOfCode.Day1 do
+
+  def solve(str) do
+    str |> String.graphemes |> Enum.map(&brace_to_int/1) |> find_basement(0, 0)
+  end
+
+  defp find_basement(arr, counter, index) do
+    if counter < 0 || index == Enum.count(arr) - 1 do
+      index
+    else
+      find_basement(arr, counter + Enum.at(arr,index), index + 1)
+    end
+  end
+
+  defp brace_to_int(x) do
+    if (x == "("), do: 1, else: -1
+  end
+
+end
+
+{ :ok, str } = File.read('./input.txt')
+IO.puts AdventOfCode.Day1.solve(str)
